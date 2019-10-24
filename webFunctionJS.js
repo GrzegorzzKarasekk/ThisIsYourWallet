02639ebce73aeca7af5a4ddcda00b8b2c24cc40e
Grzegorz Karasek <grzegorz.karasek.programista@gmail.com>
2019-10-24 21:04:49 +0200

    Added feature addIncome

---
+++ webFunctionJS.js
@@ -0,0 +1,5 @@
+$(document).ready( function() {
+    var now = new Date();
+    var today = now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate();
+    $('#datePicker').val(today);
+});
